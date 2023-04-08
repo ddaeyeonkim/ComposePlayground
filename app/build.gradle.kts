d4190a6ef53f00813844c6159f4258f43f1e5a5c
ddaeyeonkim <improve777@gmail.com>
2023-04-08 10:43:10 +0900

    Update gradle kotlin dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ktlint)
+}
+
+android {
+    namespace = "com.composeplayground"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.composeplayground"
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.4"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+ktlint {
+    android.set(true)
+    outputColorName.set("RED")
+}
+
+dependencies {
+    implementation(libs.bundles.android)
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.debug)
+
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.bundles.test.android)
+}
